fe9d4599571579f2ec0ea63bc63f65c5c41a9ed4
ivan rono <princeivan733@gmail.com>
2024-04-24 15:24:25 +0300

    first commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from "../components/Form"
+
+const Login = () => {
+    return (
+        <Form route="/api/token/" method="login" />
+    )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Form from "../components/Form"
+const Register = () => {
+    return (
+        <Form route="/api/user/register/" method="register" />
+    )
+}
+
+export default Register
